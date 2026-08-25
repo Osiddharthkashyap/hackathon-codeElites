8a12726eda0c6d418633d8575743f002dc040f6e
Osiddharthkashyap <siddharthkashyap0904@gmail.com>
2026-08-25 09:52:15 +0530

    establish the structure

---
+++ backend/app.js
@@ -0,0 +1,16 @@
+import express from "express";
+import "dotenv/config";
+
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.status(200).json({ status: "ok" });
+});
+
+app.listen(PORT, () => {
+    console.log(`Backend server running on port ${PORT}`);
+});
